e23f1a3522283a0b0775777c80b08b2c90eca065
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-21 12:39:43 +0530

    Remove dist folder from repo and add to .gitignore

--- dist/controllers/article.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare const addingArticle: (req: Request, res: Response) => Promise<void>;
-export declare const gettingSingleArticle: (req: Request, res: Response) => Promise<void>;
-export declare const gettingAllArticles: (req: Request, res: Response) => Promise<void>;
-export declare const updateArticle: (req: Request, res: Response) => Promise<void>;
-export declare const deleteArticle: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=article.controller.d.ts.map
--- dist/routes/article.route.d.ts
+++
@@ -1,3 +0,0 @@
-declare const route: import("express-serve-static-core").Router;
-export default route;
-//# sourceMappingURL=article.route.d.ts.map
