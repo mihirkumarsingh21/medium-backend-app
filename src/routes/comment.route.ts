b5c8281ea1f14f583782c3c5cc54b3b3a14d6d44
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-22 23:08:31 +0530

    import update & delete route

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/auth.middleware.js";
-import { addingComment, updateComment } from "../controllers/comment.controller.js";
+import { addingComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
 
 const route = express.Router();
 
 route.post("/add/:articleId", protectedRoute, addingComment);
 route.put("/update/:articleId", protectedRoute, updateComment);
+route.delete("/delete/:articleId", protectedRoute, deleteComment);
 
 
 
