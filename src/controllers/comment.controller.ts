78a8bead93829176efe8916afd23ec1f2f10530b
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-22 23:07:54 +0530

    added update & deleted functonalty in comment

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -78,6 +78,10 @@ export const updateComment = async (req: AuthRequest, res: Response): Promise < 
             articleId: string
         }
 
+        const {comment} = req.body as {
+            comment: string
+        }
+
         const userId = req.userId;
 
         const isArticleAndUserExsitInCommentTable = await client.comment.findFirst({
@@ -98,8 +102,32 @@ export const updateComment = async (req: AuthRequest, res: Response): Promise < 
             return;
         }
 
+        const commentId = isArticleAndUserExsitInCommentTable.id;
+
+        const updatedCommnet = await client.comment.update({
+            where: {
+                id: Number(commentId)
+            }, 
+            data: {
+                comments: comment
+            }
+        })
         
 
+        if(!updatedCommnet) {
+            res.status(400).json({
+                success: false,
+                message: "failed to update comment."
+            })
+            return;
+        }
+
+        res.status(201).json({
+            success: true,
+            message: "your comment updated successfully."
+        })
+
+        return;
 
     } catch (error: any) {
         console.log(`error while updating comment :${error.message}`);
@@ -111,3 +139,65 @@ export const updateComment = async (req: AuthRequest, res: Response): Promise < 
         return;
     }
 }
+
+
+export const deleteComment = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {
+           const {articleId} = req.params as {
+            articleId: string
+        }
+
+        const userId = req.userId;
+
+        const isArticleAndUserExsitInCommentTable = await client.comment.findFirst({
+            where: {
+                article_id: Number(articleId),
+                AND: {
+                    user_id: Number(userId)
+                }
+
+            }
+        })
+
+        if(!isArticleAndUserExsitInCommentTable) {
+            res.status(404).json({
+                success: false,
+                message: "not found : user and article does not exsit in the comment table."
+            })
+            return;
+        }
+
+        const commentId = isArticleAndUserExsitInCommentTable.id;
+
+        const deletedComment = await client.comment.delete({
+            where: {
+                id: Number(commentId)
+            }
+        })
+
+        if(!deletedComment) {
+            res.status(400).json({
+                success: false,
+                message: "failed to delete delete your comment."
+            })
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            mesaage: "your comment deleted successfully."
+        })
+        return;
+
+    } catch (error: any) {
+        console.log(`error while deleting comment: ${error.message}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong : ${error.message}`
+        })
+        return;
+    }
+}
+
+
