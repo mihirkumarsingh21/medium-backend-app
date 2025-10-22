a1d1ae6ac9e3e416c7791e2ca8efd8c9b003a8fe
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-22 10:56:51 +0530

    add some more routes init

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import { addingArticle, deleteArticle, gettingAllArticles, gettingSingleArticle, updateArticle } from "../controllers/article.controller.js";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
+import { author } from "../middlewares/author.middleware.js";
 
 const route = express.Router();
 
-route.post("/add", addingArticle);
-route.get("/single/:id", gettingSingleArticle);
-route.get("/all", gettingAllArticles);
-route.put("/update/:id", updateArticle);
-route.delete("/delete/:id", deleteArticle);
+route.post("/add", protectedRoute, author, addingArticle);
+route.put("/update/:id", protectedRoute, author, updateArticle);
+route.delete("/delete/:id", protectedRoute, author, deleteArticle);
+
+
+route.get("/single/:id", protectedRoute, gettingSingleArticle);
+route.get("/all", protectedRoute, gettingAllArticles);
+
+
 
 
 export default route;
