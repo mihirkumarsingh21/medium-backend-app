fcbd0ada3e92e8b8f06acd410a6aab339be5fa28
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-22 21:57:44 +0530

    updated index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express  from "express";
 import dotenv from "dotenv";
-import articleRoute from "./routes/article.route.js"
-import authRoute from "./routes/user.route.js"
+import articleRoute from "./routes/article.route.js";
+import authRoute from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import commentRoute from "./routes/comment.route.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 app.use("/medium/api/v1/users", authRoute);
 
 app.use("/medium/api/v1/articles", articleRoute)
+app.use("/medium/api/v1/comments", commentRoute);
 
 const PORT = process.env.PORT;
 
