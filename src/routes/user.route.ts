391315fdc58ba536873d5c0449fe07cc2b33c286
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-22 21:59:19 +0530

    modified user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { loginUser, logoutUser, makingAuthor, registerUser } from "../controllers/user.controller.js";
+import { author } from "../middlewares/author.middleware.js";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
 
 
 const route = express.Router();
@@ -7,7 +9,7 @@ const route = express.Router();
 route.post("/register", registerUser);
 route.post("/login", loginUser);
 route.get("/logout", logoutUser);
-route.patch("/make-author/:userId", makingAuthor);
+route.patch("/make-author/:userId", protectedRoute, author, makingAuthor);
 
 
 export default route;
