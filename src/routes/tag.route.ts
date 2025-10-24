5ae2cb1d2dae0732e10d8c7d10ebc6948aa80ee9
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-24 16:31:15 +0530

    modified tag route

--- src/routes/tag.route.ts
+++ src/routes/tag.route.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { protectedRoute } from '../middlewares/auth.middleware.js';
 import { author } from '../middlewares/author.middleware.js';
-import { addTag } from '../controllers/tag.controller.js';
+import { addTag, deleteTag, updateTag } from '../controllers/tag.controller.js';
 
 const route = express.Router();
 
 
 route.post("/add/:articleId", protectedRoute, author, addTag);
+route.patch("/update-tag/article/:articleId/tag/:tagId", protectedRoute, author, updateTag);
+route.delete("/delete/:tagId", protectedRoute, author, deleteTag);
+
+// route.patch("/update-tags/articles/:articleId/tags/:tagId", protectedRoute, author, updateTags);
+
 
 
 export default route;
