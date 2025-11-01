275abaf90614bfb997282cc5d8800163e3bd60a9
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-11-01 05:44:16 +0530

    follow routes

--- src/routes/follow.route.ts
+++ src/routes/follow.route.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/auth.middleware.js";
-import { follow } from "../controllers/follow.controller.js";
+import { follow, gettingAuthorFollowings, gettingAuthorSingleFollowers, gettingAuthorSingleFollowings } from "../controllers/follow.controller.js";
+import { gettingAuthorFollowers } from "../controllers/follow.controller.js";
 
 
 const route = express.Router();
 
-route.post("/author/autherId", protectedRoute, follow);
+route.post("/author/:autherId", protectedRoute, follow);
+route.get("/author/followers", protectedRoute, gettingAuthorFollowers);
+route.get("/user/:userId/followers", protectedRoute, gettingAuthorSingleFollowers);
+route.get("/author/followings", protectedRoute, gettingAuthorFollowings);
+route.get("/user/:userId/followings", protectedRoute, gettingAuthorSingleFollowings);
+
+
+
 
 
 export default route;
